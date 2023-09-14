a1c8a30c4e1906ede2e86b6000bbff74ed042765
Till Ziegler <tz@schoeneweide.tk>
2023-09-14 21:07:24 +0200

    add members query message

--- src/query.rs
+++ src/query.rs
@@ -1,9 +1,16 @@
 use cosmwasm_schema::{cw_serde, QueryResponses};
-use crate::state::{ContractConfig};
+use cosmwasm_std::{Addr};
 
 #[cw_serde]
 #[derive(QueryResponses)]
 pub enum QueryMsg {
     #[returns(ContractConfig)]
     Config {},
+    #[returns(QueryMembersResponse)]
+    Members {},
+}
+
+#[cw_serde]
+pub struct QueryMembersResponse {
+    pub members: Vec<Addr>,
 }
